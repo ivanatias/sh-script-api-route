5104e525feb16e5b69ffd73167af8ca016f52e64
Ivan Atias <ivan.d.atias@gmail.com>
2023-06-01 16:44:27 -0400

    update relevant files on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <main className="flex flex-col max-w-lg gap-5 p-8 mx-auto">
       <h1 className="text-2xl font-bold text-center">
-        Running shell script on API route and streaming output to the
-        client-side
+        Running shell script on API route and route handler, and streaming
+        output to the client-side
       </h1>
       <p className="text-lg font-semibold text-center">Relevant files:</p>
       <div className="flex flex-wrap justify-center gap-2">
         <Code code="scripts/example.sh" />
-        <Code code="components/output.tsx" />
+        <Code code="components/results-output.tsx" />
         <Code code="pages/api/process.ts" />
         <Code code="app/apiv2/route.ts" />
       </div>
